6a97e512732b90cab1b4d08e8d4a985888fccd0b
U180709023 <sogood542>
2020-09-29 13:47:51 +0300

    change display of user in admin site

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import User,Column,Post
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email',)
 admin.site.register(Column)
 admin.site.register(Post)
